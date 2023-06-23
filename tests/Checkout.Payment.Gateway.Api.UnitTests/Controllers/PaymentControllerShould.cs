b84e272881824d2b05d7f1a3ff45b9da43e377f3
vpaskevicus <>
2023-06-23 11:16:45 +0100

    add payment request fixture for test code reuse and refactoring

--- tests/Checkout.Payment.Gateway.Api.UnitTests/Controllers/PaymentControllerShould.cs
+++ tests/Checkout.Payment.Gateway.Api.UnitTests/Controllers/PaymentControllerShould.cs
@@ -1,49 +1,36 @@
 ﻿using Checkout.Payment.Gateway.Api.Contracts.Requests;
 using Checkout.Payment.Gateway.Api.Controllers;
 using Checkout.Payment.Gateway.Api.Services;
-using Microsoft.AspNetCore.Http;
+using Checkout.Payment.Gateway.Api.UnitTests.Fixtures;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
-using CardDetails = Checkout.Payment.Gateway.Api.Contracts.CardDetails;
-
 namespace Checkout.Payment.Gateway.Api.UnitTests.Controllers
 {
+    [Collection("UnitTestFixtures")]
     public class PaymentControllerShould
     {
         private readonly PaymentController _paymentController;
 
+        private readonly PaymentRequest _basicPaymentRequest;
+
         private readonly Mock<IPaymentService> _paymentServiceMock;
 
-        public PaymentControllerShould() 
+        public PaymentControllerShould(PaymentRequestFixture paymentRequestFixtures) 
         {
             _paymentServiceMock = new Mock<IPaymentService>();
 
             _paymentController = new PaymentController(_paymentServiceMock.Object);
+
+            _basicPaymentRequest = paymentRequestFixtures.BasicPaymentRequest;
         }
 
         [Fact]
         public async Task Return200OKWhenPaymentCreatedSucessfully()
         {
-            var paymentRequest = new PaymentRequest()
-            {
-                ShopperId = Guid.NewGuid(),
-                MerchantId = Guid.NewGuid(),
-                Currency = "gbp",
-                Amount = 156.60m,
-                ShopperCardDetails = new CardDetails()
-                {
-                    NameOnCard = "Vladimirs Paskevicus",
-                    CardNumber = "1243123412341234",
-                    ExpirationMonth = 3,
-                    ExpirationYear = 2027,
-                    SecurityCode = 555
-                }
-            };
-
             _paymentServiceMock.Setup(m => m.ProcessPaymentAsync(It.IsAny<Models.Payment>())).ReturnsAsync(true);
 
-            var response = await _paymentController.CreatePayment(paymentRequest);
+            var response = await _paymentController.CreatePayment(_basicPaymentRequest);
 
             ((StatusCodeResult)response).StatusCode.Should().Be(200);
         }
@@ -51,25 +38,9 @@ namespace Checkout.Payment.Gateway.Api.UnitTests.Controllers
         [Fact]
         public async Task Return500InternalServerErrorIfPaymentServiceReturnsUnsucessful()
         {
-            var paymentRequest = new PaymentRequest()
-            {
-                ShopperId = Guid.NewGuid(),
-                MerchantId = Guid.NewGuid(),
-                Currency = "gbp",
-                Amount = 156.60m,
-                ShopperCardDetails = new CardDetails()
-                {
-                    NameOnCard = "Vladimirs Paskevicus",
-                    CardNumber = "1243123412341234",
-                    ExpirationMonth = 3,
-                    ExpirationYear = 2027,
-                    SecurityCode = 555
-                }
-            };
-
             _paymentServiceMock.Setup(m => m.ProcessPaymentAsync(It.IsAny<Models.Payment>())).ReturnsAsync(false);
 
-            var response = await _paymentController.CreatePayment(paymentRequest);
+            var response = await _paymentController.CreatePayment(_basicPaymentRequest);
 
             ((StatusCodeResult)response).StatusCode.Should().Be(500);
         }
@@ -77,25 +48,9 @@ namespace Checkout.Payment.Gateway.Api.UnitTests.Controllers
         [Fact]
         public async Task Return500InternalServerErrorIfPaymentServiceThrowsException()
         {
-            var paymentRequest = new PaymentRequest()
-            {
-                ShopperId = Guid.NewGuid(),
-                MerchantId = Guid.NewGuid(),
-                Currency = "gbp",
-                Amount = 156.60m,
-                ShopperCardDetails = new CardDetails()
-                {
-                    NameOnCard = "Vladimirs Paskevicus",
-                    CardNumber = "1243123412341234",
-                    ExpirationMonth = 3,
-                    ExpirationYear = 2027,
-                    SecurityCode = 555
-                }
-            };
-
             _paymentServiceMock.Setup(m => m.ProcessPaymentAsync(It.IsAny<Models.Payment>())).Throws(new Exception("something went wrong!"));
 
-            var response = await _paymentController.CreatePayment(paymentRequest);
+            var response = await _paymentController.CreatePayment(_basicPaymentRequest);
 
             ((StatusCodeResult)response).StatusCode.Should().Be(500);
         }
